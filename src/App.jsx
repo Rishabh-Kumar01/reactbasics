4c53ac2fe10716f453e9dc2e9d2c059028d9ca2a
Rishabh-Kumar01 <rishabh1teotia@gmail.com>
2024-12-11 20:17:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello World</div>
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+function Button() {
+  return <button>Click me</button>;
+}
+
+export default App;
